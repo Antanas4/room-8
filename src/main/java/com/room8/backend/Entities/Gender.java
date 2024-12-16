d294fcaac8434fa0e44831faabbf241a2fe42300
confusedGustas <gustas.antanas2003@gmail.com>
2024-12-16 12:37:08 +0200

    database

---
+++ src/main/java/com/room8/backend/Entities/Gender.java
@@ -0,0 +1,5 @@
+package com.room8.backend.Entities;
+
+public enum Gender {
+    MALE, FEMALE
+}
---
+++ src/main/java/com/room8/backend/Entities/User.java
@@ -0,0 +1,38 @@
+package com.room8.backend.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+}
